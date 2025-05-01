51d60eff77aa06e033b0fbf42aad1e625ff5f113
MedusaTea <medusastea@gmail.com>
2025-05-01 15:41:09 -0500

    init

---
+++ bot.py
@@ -0,0 +1,29 @@
+
+from twitchio.ext import commands
+import os
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        super().__init__(
+            token=os.environ['TMI_TOKEN'],
+            prefix='!',
+            initial_channels=[os.environ['CHANNEL']]
+        )
+
+    async def event_ready(self):
+        print(f'Logged in as | {self.nick}')
+
+    async def event_message(self, message):
+        if message.echo:
+            return
+        await self.handle_commands(message)
+
+    @commands.command(name='hello')
+    async def my_command(self, ctx):
+        await ctx.send(f'Hello @{ctx.author.name}!')
+
+if __name__ == "__main__":
+    bot = Bot()
+    bot.run()
+
